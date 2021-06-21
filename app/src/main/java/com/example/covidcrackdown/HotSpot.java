ef91bd2d7b7a61326e2ffe39d098a9bdcc50c343
shawn <shawnang167@gmail.com>
2021-06-22 00:11:55 +0800

    hotspot completed

--- app/src/main/java/com/example/covidcrackdown/HotSpot.java
+++ app/src/main/java/com/example/covidcrackdown/HotSpot.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -42,7 +45,28 @@ public class HotSpot extends AppCompatActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
 
         googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
+                .position(new LatLng(2.8327465697628815, 101.70697499691381))
                 .title("Marker"));
+
+        googleMap.addCircle(new CircleOptions()
+                .center(new LatLng(2.8370114208250823, 101.70636345331911))
+                .radius(300)
+                .strokeWidth(10)
+                .strokeColor(Color.argb(128, 255, 0, 0))
+                .fillColor(Color.argb(128, 255, 0, 0)));
+
+        googleMap.addCircle(new CircleOptions()
+                .center(new LatLng(2.824554296864241, 101.69383890174753))
+                .radius(500)
+                .strokeWidth(10)
+                .strokeColor(Color.argb(128, 255, 0, 0))
+                .fillColor(Color.argb(128, 255, 0, 0)));
+
+        googleMap.addCircle(new CircleOptions()
+                .center(new LatLng(2.843081226805592, 101.68329483365896))
+                .radius(800)
+                .strokeWidth(10)
+                .strokeColor(Color.argb(128, 255, 0, 0))
+                .fillColor(Color.argb(128, 255, 0, 0)));
     }
 }
